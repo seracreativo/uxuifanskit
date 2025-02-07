b21e1d07fda4179beda3809bac0f619dd59e3e5b
Frank Sera <78000380+seracreativo@users.noreply.github.com>
2025-02-07 07:56:25 +0100

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 
 const Button = ({ children, onClick }) => {
 	return (
-		<button
-			className='px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition'
-			onClick={onClick}
-		>
+		<button id='button' onClick={onClick}>
 			{children}
 		</button>
 	);
