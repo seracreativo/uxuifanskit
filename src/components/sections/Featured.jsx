a9c2b53d000222d43713720875a7f2d4f1d5b7dd
Frank Sera <78000380+seracreativo@users.noreply.github.com>
2025-03-01 00:48:08 +0100

    -- fix

--- src/components/sections/Featured.jsx
+++ src/components/sections/Featured.jsx
@@ -4,7 +4,7 @@ const Featured = ({ className = '', ...props }) => {
 	return (
 		<div className='grid  divide-x  divide-border border-y border-border text-body grid-cols-1 grid-rows-1 md:grid-cols-3 md:grid-rows-2 lg:grid-rows-1 lg:grid-cols-5'>
 			<div className='p-4 flex items-center justify-center  md:row-span-full bg-surface'>
-				<p className='text-2xl'>Featured by</p>
+				<p className='text-2xl'>Featured by Fernan</p>
 			</div>
 			<div className='p-4 flex items-center justify-center border-t border-border md:border-0'>
 				<img
